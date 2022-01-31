88d948f0c16f15c7d892952afaad93de2330bf4e
moochezz <78286979+moochezz@users.noreply.github.com>
2022-01-31 15:00:33 -0500

    Finalized Code

--- main.cpp
+++ main.cpp
@@ -34,16 +34,19 @@ int* getnum() //return type- address of integer array
     return arr; //returning the address of the array holding the numbers
 }
 int main()
-{
+{   
     int op; // the variable which holds the operation the user would like 
-    int i; // variable that will be used inside of a for loop; will be explained later as to why we're using this
-    int num = 0; // the initial number that will be added to once we start the math
-
-    cout << "\n Welcome to the C++ scientific calculator! What operation would you like to complete (Choose corresponding number)? \n 1. Addition \n 2. Subtraction \n 3. Multiplication \n 4. Division \n 5. x^2 \n 6. x^3 \n 7. sqrt \n 8. cbrt \n 9. x^y \n 10. SA of rect prism \n 11. V of rect prism \n 12. SA of prism \n 13. V of prism \n 14. SA of pyramid \n 15. V of pyramid \n";
+    int i;
+    cout << "\n Welcome to the C++ scientific calculator! What operation would you like to complete (Choose corresponding number)? \n 1. Addition \n 2. Subtraction \n 3. Multiplication \n 4. Division \n 5. x^2 (set # of numbers to 1) \n 6. x^3 (set # of numbers to 1) \n 7. sqrt (set # of numbers to 1) \n 8. cbrt (set # of numbers to 1) \n 9. x^y (set # of numbers to 2)\n 10. SA of rect prism (set # of numbers to 3 (number inputs are b, h, & l))\n 11. V of rect prism (set # of numbers to 3 (number inputs are b, h, & l))\n 12. V of Square Pyramid (set # of numbers to 2 (number inputs are b, & h)) \n";
     cin >> op; // sends the result of the user input to the operation variable
+    int* numArray; //pointer to hold address
+    numArray = getnum(); //address of the array
+    // variable that will be used inside of a for loop; will be explained later as to why we're using this
+    float num = 0; // the initial number that will be added to once we start the math
+    float b = numArray[0];
+    float h = numArray[1];
+    float l = numArray[2];
     
-    int* ptr; //pointer to hold address
-    ptr = getnum(); //address of the array
     
     
     // now we do the actual math
@@ -51,7 +54,7 @@ int main()
     { 
         for(i = 0; i<numcount; i++)
         {
-            num = num+ptr[i];
+            num = num+numArray[i];
         }
     } 
     else if (op == 2) // subtraction
@@ -61,13 +64,13 @@ int main()
 
             if (i == 0)
             {
-               num = ptr[0]; // checks if this is the first number we're using, and if so, makes that the number variable (we dont wanna subtract from zero)
+               num = numArray[0]; // checks if this is the first number we're using, and if so, makes that the number variable (we dont wanna subtract from zero)
             } 
             
 
             else
             {
-            num = num-ptr[i]; // if it isnt the first number, simply subtract
+            num = num-numArray[i]; // if it isnt the first number, simply subtract
             }
         
 
@@ -81,13 +84,13 @@ int main()
 
             if (i == 0)
             {
-               num = ptr[0]; // checks if this is the first number we're using, and if so, makes that the number variable
+               num = numArray[0]; // checks if this is the first number we're using, and if so, makes that the number variable
             } 
             
 
             else
             {
-            num = num*ptr[i]; // if it isnt the first number, multiply
+            num = num*numArray[i]; // if it isnt the first number, multiply
             }
         
 
@@ -101,13 +104,13 @@ int main()
 
             if (i == 0)
             {
-               num = ptr[0];
+               num = numArray[0];
             } 
             
 
             else
             {
-            num = num/ptr[i];
+            num = num/numArray[i];
             }
         
 
@@ -116,22 +119,29 @@ int main()
     }
     else if (op == 5) // squared
     {
-        num = pow(ptr[0], 2); // the function pow is a part of the "cmath" library, short form for power; the first argument is the base, the second number is the exponent
+        num = pow(numArray[0], 2); // the function pow is a part of the "cmath" library, short form for power; the first argument is the base, the second number is the exponent
     }
     else if(op == 6){ // cubed
-        num = pow(ptr[0], 3);
+        num = pow(numArray[0], 3);
     }
     else if(op == 7){ // square root
-        num = sqrt(ptr[0]);
+        num = sqrt(numArray[0]);
     }
     else if(op == 8){ // cube root
-        num = cbrt(ptr[0]);
+        num = cbrt(numArray[0]);
     }
     else if(op == 9){
-        num = pow(ptr[0], ptr[1]);
+        num = pow(numArray[0], numArray[1]);
     }
     else if(op == 10){
-
+        num = b*h*2+b*l*2+h*l*2;
     }
-
+    else if(op == 11){
+        num = b*h*l;
+    }
+    else if(op == 12){
+        num = b*h/3;
+    }
+   
     cout << "The result is " << num; // finally, prints out the answer
+}
