1f76131e4a8e4f37f5d251ea6f9f104c16c81a8b
frfreezz <98795384+frfreezz@users.noreply.github.com>
2022-02-01 09:40:30 -0500

    Finalized Code

--- main.cpp
+++ main.cpp
@@ -2,12 +2,12 @@
 #include <string>
 #include <cmath> // library used for math function such as square root, cube root, and power
 
+
 using namespace std;
 
 int numcount; 
 int* getnum() //return type- address of integer array
 {
-    int op;
 
     cout << "How many numbers would you like?\n";
     cin >> numcount;
@@ -22,11 +22,7 @@ int* getnum() //return type- address of integer array
             cout << "What is your starting number?\n";
             cin >> arr[i];
         }
-        if (i > 0)
-        {
-            cout << "What is the next number?\n";
-            cin >> arr[i];
-        }
+        
     }
     
     return arr; //returning the address of the array holding the numbers
@@ -37,13 +33,19 @@ int main()
     int i;
     cout << "\n Welcome to the C++ scientific calculator! What operation would you like to complete (Choose corresponding number)? \n 1. Addition \n 2. Subtraction \n 3. Multiplication \n 4. Division \n 5. x^2 (set # of numbers to 1) \n 6. x^3 (set # of numbers to 1) \n 7. sqrt (set # of numbers to 1) \n 8. cbrt (set # of numbers to 1) \n 9. x^y (set # of numbers to 2)\n 10. SA of rect prism (set # of numbers to 3 (number inputs are b, h, & l))\n 11. V of rect prism (set # of numbers to 3 (number inputs are b, h, & l))\n 12. V of Square Pyramid (set # of numbers to 2 (number inputs are b, & h)) \n";
     cin >> op; // sends the result of the user input to the operation variable
-    int* numArray; //pointer to hold address
-    numArray = getnum(); //address of the array
+    if (op > 12)
+    {
+        cout << "PUT IN EI GDODO VAULE  BROIH >:(";
+    }
+    return 0;
+    int* ptr; //pointer to hold address
+    ptr = getnum(); //address of the array
     // variable that will be used inside of a for loop; will be explained later as to why we're using this
     float num = 0; // the initial number that will be added to once we start the math
-    float b = numArray[0];
-    float h = numArray[1];
-    float l = numArray[2];
+    float b = ptr[0];
+    float h = ptr[1];
+    float l = ptr[2];
+    
     
     
     // now we do the actual math
@@ -51,37 +53,45 @@ int main()
     { 
         for(i = 0; i<numcount; i++)
         {
-            num = num+numArray[i];
+            num = num+ptr[i];
         }
     } 
     else if (op == 2) // subtraction
     {
         for(i = 0; i<numcount; i++)
         {
+
             if (i == 0)
             {
-               num = numArray[0]; // checks if this is the first number we're using, and if so, makes that the number variable (we dont wanna subtract from zero)
+               num = ptr[0]; // checks if this is the first number we're using, and if so, makes that the number variable (we dont wanna subtract from zero)
             } 
+            
 
             else
             {
-            num = num-numArray[i]; // if it isnt the first number, simply subtract
+            num = num-ptr[i]; // if it isnt the first number, simply subtract
             }
+        
+
          }
+    
     }
     else if (op == 3) // multiplication
     {
         for(i = 0; i<numcount; i++)
         {
+
             if (i == 0)
             {
-               num = numArray[0]; // checks if this is the first number we're using, and if so, makes that the number variable
+               num = ptr[0]; // checks if this is the first number we're using, and if so, makes that the number variable
             } 
             
+
             else
             {
-            num = num*numArray[i]; // if it isnt the first number, multiply
+            num = num*ptr[i]; // if it isnt the first number, multiply
             }
+        
 
          }
     
@@ -93,30 +103,34 @@ int main()
 
             if (i == 0)
             {
-               num = numArray[0];
+               num = ptr[0];
             } 
             
+
             else
             {
-            num = num/numArray[i];
+            num = num/ptr[i];
             }
+        
+
          }
+    
     }
     else if (op == 5) // squared
     {
-        num = pow(numArray[0], 2); // the function pow is a part of the "cmath" library, short form for power; the first argument is the base, the second number is the exponent
+        num = pow(ptr[0], 2); // the function pow is a part of the "cmath" library, short form for power; the first argument is the base, the second number is the exponent
     }
     else if(op == 6){ // cubed
-        num = pow(numArray[0], 3);
+        num = pow(ptr[0], 3);
     }
     else if(op == 7){ // square root
-        num = sqrt(numArray[0]);
+        num = sqrt(ptr[0]);
     }
     else if(op == 8){ // cube root
-        num = cbrt(numArray[0]);
+        num = cbrt(ptr[0]);
     }
     else if(op == 9){
-        num = pow(numArray[0], numArray[1]);
+        num = pow(ptr[0], ptr[1]);
     }
     else if(op == 10){
         num = b*h*2+b*l*2+h*l*2;
@@ -125,8 +139,9 @@ int main()
         num = b*h*l;
     }
     else if(op == 12){
-        num = b*h/3;
+        num = (b*b)*h/3;
     }
+        
    
     cout << "The result is " << num; // finally, prints out the answer
 }
